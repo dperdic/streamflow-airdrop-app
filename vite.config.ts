17efe59c8202e9f53e64dc658d808ce3b24465c2
Dujo Perdic <dujo.perdic1@gmail.com>
2025-03-10 13:08:33 +0100

    import organizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
