5019a7518b2507e54667ed1ccce2483eb4cb964f
Dujo Perdic <dujo@alakazam.io>
2025-05-19 16:48:38 +0200

    layout wip

--- src/components/layout/Main.tsx
+++ src/components/layout/Main.tsx
@@ -1,13 +1,39 @@
 import Home from "@components/Home";
+import { useWallet } from "@solana/wallet-adapter-react";
+import { Slide, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Main() {
+  const { publicKey } = useWallet();
   return (
-    <main className="mt-18 flex h-full w-full grow justify-center px-8 py-8 sm:px-16">
-      <div className="mx-auto w-full max-w-4xl">
-        <main className="flex flex-col items-center p-8">
+    <main className="mt-18 flex h-full w-full flex-grow items-center justify-center px-8 py-8 sm:px-16">
+      {publicKey ? (
+        <div className="mx-auto grid w-full max-w-7xl gap-12">
           <Home />
-        </main>
-      </div>
+        </div>
+      ) : (
+        <div className="mx-auto w-full max-w-2xl">
+          <div className="w-full rounded-md bg-white p-8 text-center shadow">
+            <h3 className="text-xl font-semibold">
+              Connect a wallet to continue
+            </h3>
+          </div>
+        </div>
+      )}
+
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable={false}
+        pauseOnHover={true}
+        theme="light"
+        transition={Slide}
+      />
     </main>
   );
 }
