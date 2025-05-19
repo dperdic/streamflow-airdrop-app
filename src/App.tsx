13d01f99a8cc19be85177b402c5611040e13ee28
Dujo Perdic <dujo@alakazam.io>
2025-05-19 16:20:38 +0200

    whitespace

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Footer from "@components/layout/Footer";
 import Header from "@components/layout/Header";
 import Main from "@components/layout/Main";
 import Providers from "@providers/Providers";
+
 export default function App() {
   return (
     <Providers>
