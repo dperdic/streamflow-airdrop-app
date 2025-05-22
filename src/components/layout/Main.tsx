28da499fcae5261177036dc10fc1a9d849859503
Dujo Perdic <dujo@alakazam.io>
2025-05-22 19:13:36 +0200

    layout and routing

--- src/components/layout/Main.tsx
+++ src/components/layout/Main.tsx
@@ -1,25 +1,20 @@
+import AirdropDetails from "@components/AirdropDetails";
 import Home from "@components/Home";
-import { useWallet } from "@solana/wallet-adapter-react";
+import NotFound from "@components/NotFound";
+import { Route, Routes } from "react-router-dom";
 import { Slide, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 export default function Main() {
-  const { publicKey } = useWallet();
   return (
-    <main className="mt-18 flex h-full w-full flex-grow items-center justify-center px-8 py-8 sm:px-16">
-      {publicKey ? (
-        <div className="mx-auto grid w-full max-w-7xl gap-12">
-          <Home />
-        </div>
-      ) : (
-        <div className="mx-auto w-full max-w-2xl">
-          <div className="w-full rounded-md bg-white p-8 text-center shadow">
-            <h3 className="text-xl font-semibold">
-              Connect a wallet to continue
-            </h3>
-          </div>
-        </div>
-      )}
+    <main className="mt-18 flex h-full w-full flex-grow px-8 py-8 sm:px-16">
+      <div className="mx-auto w-full max-w-7xl">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/airdrops/:id" element={<AirdropDetails />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
 
       <ToastContainer
         position="top-right"
