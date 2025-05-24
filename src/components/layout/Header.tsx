4e802b46b67092feec3589770b36a218f199cb7f
Dujo Perdic <dujo@alakazam.io>
2025-05-24 15:37:53 +0200

    fixed header navigation

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,11 +2,9 @@ import { useConnection, useWallet } from "@solana/wallet-adapter-react";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import { LAMPORTS_PER_SOL } from "@solana/web3.js";
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
 
 export default function Header() {
   const [balance, setBalance] = useState(0);
-  const navigate = useNavigate();
 
   const { connection } = useConnection();
   const { publicKey } = useWallet();
@@ -36,12 +34,9 @@ export default function Header() {
   return (
     <header className="fixed top-0 z-10 flex h-18 w-full border-b bg-white shadow-xs">
       <nav className="flex h-full w-full items-center justify-between gap-4 px-8 sm:px-16">
-        <img
-          src="/vite.svg"
-          alt="Vite"
-          className="size-10 cursor-pointer"
-          onClick={() => navigate("/")}
-        />
+        <a href="/">
+          <img src="/vite.svg" alt="Vite" className="size-10 cursor-pointer" />
+        </a>
         <div className="flex gap-4">
           <span className="my-auto">Balance: {balance} SOL</span>
           <WalletMultiButton className="my-auto" />
