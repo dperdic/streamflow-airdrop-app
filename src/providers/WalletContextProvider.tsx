3ee629369596f757d15ae8a16b1c8aaf296b5a83
Dujo Perdic <dujo@alakazam.io>
2025-05-25 19:09:38 +0200

    removed useless vars

--- src/providers/WalletContextProvider.tsx
+++ src/providers/WalletContextProvider.tsx
@@ -4,15 +4,10 @@ import {
 } from "@solana/wallet-adapter-react";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import "@solana/wallet-adapter-react-ui/styles.css";
-import { Cluster, clusterApiUrl } from "@solana/web3.js";
 import { ReactNode } from "react";
 
 const getEndpoint = (): string => {
   const rpcUrl = import.meta.env.VITE_RPC_URL;
-
-  if (!rpcUrl || rpcUrl === "") {
-    return clusterApiUrl(import.meta.env.VITE_SOL_CLUSTER as Cluster);
-  }
 
   return rpcUrl;
 };
